94e1042b66c57d0ea3745f655c5074d9d4d195c4
Tiago2903 <xaloaxolot@gmail.com>
2023-07-29 22:15:03 -0300

    continuação modulo

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import http from 'http'
 import { json } from './middlewares/json.js'
+import { Database } from './middlewares/database.js'
 
 // node src/server.js
 // npm start
 
-const users = [];
+const database = new Database;
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
@@ -12,16 +13,19 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
     if (method === "GET" && url === '/users') {
-        return res
-            .end(JSON.stringify(users));
+        const users = database.select('users')
+        return res.end(JSON.stringify(users));
     }
+
     if (method === "POST" && url === '/users') {
         const { name, email } = req.fullBody;
-        users.push({
+        const user = {
             id: 1,
             name,
             email
-        });
+        };
+
+        database.insert('users', user)
     }
 
     return res.end('mundo criado');

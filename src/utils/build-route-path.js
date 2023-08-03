0c096951aab56336f573b1d3452ebccc32322f72
Tiago2903 <xaloaxolot@gmail.com>
2023-08-03 17:49:17 -0300

    final modulo 5

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,7 +1,7 @@
 export function buildRoutePath(path) {
     const routeParamRegex = /:([a-zA-Z]+)/g
     const pathWithParams = path.replaceAll(routeParamRegex, '(?<$1>[a-z0-9\-_]+)');
-    const pathRegex = new RegExp(`^${pathWithParams}`);
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
     return pathRegex;
 }
